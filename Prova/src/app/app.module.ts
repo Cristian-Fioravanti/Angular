46c848cd01f5a22e4c050e8f7eb970c8e31c79dc
LAPTOP-O2AHG926\Mice <cristian.fioravanti@micegroup.it>
2022-12-14 14:34:35 +0100

    Finita week 2

--- Prova/src/app/app.module.ts
+++ Prova/src/app/app.module.ts
@@ -18,6 +18,8 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import {PromotionService} from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 
 @NgModule({
@@ -42,7 +44,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatButtonModule,
     MatListModule
   ],
-  providers: [DishService],
+  providers: [DishService,PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
